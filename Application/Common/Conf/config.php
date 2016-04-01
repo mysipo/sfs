9a70de85c7514501f44dd145c44c3c75edf7d748
小龙 <864566775@qq.com>
2016-04-01 16:41:20 +0800

    update config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -39,10 +39,10 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '101.200.111.51', // 服务器地址
-    'DB_NAME'   => 'mysipo_video', // 数据库名
-    'DB_USER'   => 'mysipo2016', // 用户名
-    'DB_PWD'    => 'mysipoadmin2016',  // 密码
+    'DB_HOST'   => '121.41.53.60', // 服务器地址
+    'DB_NAME'   => 'onethink', // 数据库名
+    'DB_USER'   => 'root ', // 用户名
+    'DB_PWD'    => 'df43fa6c89',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'lesson_', // 数据库表前缀
 
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '0c14O/ujcYn1uu6DWKJMrh3ynGxeQ10rElp4iMY'); //加密KEY
-define('UC_DB_DSN', 'mysql://mysipo2016:mysipoadmin2016@101.200.111.51:3306/mysipo_bbs'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:df43fa6c89@121.41.53.60:3306/mysipo_bbs'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'mys_'); // 数据表前缀，使用Model方式调用API必须配置此项
